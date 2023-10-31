3b8e5ba87882022ee2ea0b329097fe9820b6a611
shulika <illia.shulika@gmail.com>
2023-10-30 18:49:08 -0700

    lesson1-preview2 //props//

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -4,7 +4,8 @@ import FirstComponent from "./components/first-component/FirstComponent";
 const App = () => {
     return (
         <div>
-            <FirstComponent/>
+            <FirstComponent txt={'Bart Simpson'} image={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
+            <FirstComponent txt={'Homer Simpson'} image={'https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png'}/>
         </div>
     );
 };
--- react/qbickk/src/components/first-component/FirstComponent.js
+++ react/qbickk/src/components/first-component/FirstComponent.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './FirstComponent.module.css';
 
-const FirstComponent = () => {
+const FirstComponent = (props) => {
+
+    const {txt,image} = props
+
+    console.log(props)
+    console.log(txt)
+    console.log(image)
+
     return (
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, amet earum eligendi excepturi fugiat in, iusto quae reiciendis rerum sit tempore, voluptatem! Adipisci aut, ex excepturi explicabo magni officia officiis, pariatur, quam quibusdam quod tempore vero. Aliquam aperiam atque commodi dolore eum inventore ipsam minus modi, nemo officiis praesentium quisquam?</p>
-
-            <img src={"https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png"} alt="qweqe"/>
+            <p>{txt}</p>
+            <img src={image} alt={'qwerty'}/>
             <hr/>
         </div>
     );

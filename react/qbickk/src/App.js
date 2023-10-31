38147a57c9d1d27b2d790351d38f2e1460191831
shulika <illia.shulika@gmail.com>
2023-10-31 16:51:48 -0700

    lesson1-preview4 // useState useEffect //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 const App = () => {
+
+    // const [users, setUsers] = useState([]);
+    // users => getter => отримуємо значення
+    // setUsers => setter => впроваджуємо значення
+
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(value => value.json())
+    //         .then(value => {
+    //             console.log('22');
+    //             setUsers(value)
+    //         })
+    // }, []);
+
+    let [counter, setCounter] = useState(0);
+    const onClickIncrement = () => {
+        counter++;
+        setCounter(counter);
+    };
+    const onClickDecrement = () => {
+        counter--;
+        setCounter(counter);
+    }
+
     return (
         <div>
-            App
+            <h1>counter: {counter}</h1>
+            <button onClick={onClickIncrement}>+</button>
+            <button onClick={onClickDecrement}>-</button>
         </div>
     );
 };

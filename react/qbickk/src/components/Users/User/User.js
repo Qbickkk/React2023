b6ae7ac0a1501df88026f07a31d6dbbcbb07eebe
shulika <illia.shulika@gmail.com>
2023-11-01 14:19:01 -0700

    lesson1-hw2

--- react/qbickk/src/components/Users/User/User.js
+++ react/qbickk/src/components/Users/User/User.js
@@ -4,6 +4,7 @@ const User = ({character}) => {
     const {id,name,status,species,gender,image} = character
     return (
         <div>
+            <h4>{id}</h4>
             <h2>{name}</h2>
             <h3>{status}</h3>
             <h3>{species}</h3>

9af936a291e867fd3981f152a680d4998688e0ff
shulika <illia.shulika@gmail.com>
2023-11-03 20:05:23 -0700

    lesson2-preview2 //axios services//

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from "axios";
+import {getAllUsers, getById} from "./services/axiosService";
+
 
 const App = () => {
+
+    useEffect(() => {
+        getAllUsers().then(value => console.log(value.data));
+        getById(9).then(value => console.log(value));
+    }, []);
+
+
     return (
         <div>
             App

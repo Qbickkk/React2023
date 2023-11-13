bc9d2522c19ebda171d0c300331003dd954b6b62
shulika <illia.shulika@gmail.com>
2023-11-13 15:13:42 -0800

    lesson3-preview3 // forms useForm //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
 
 const App = () => {
+
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+
     return (
         <div>
-            App
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register('title', {required: true})}/>
+                <input {...register('body')}/>
+                <button>Save Post</button>
+            </form>
         </div>
     );
 };

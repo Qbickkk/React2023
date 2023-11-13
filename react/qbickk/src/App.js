6ff6602284df5765d3e626a08070a4129eb53386
shulika <illia.shulika@gmail.com>
2023-11-13 14:32:16 -0800

    lesson3-preview2 // forms createRef //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {createRef} from 'react';
 
 const App = () => {
+    const login = createRef();
+    const password = createRef();
+
+    const onSubmit = (e) => {
+        console.log(login.current.value);
+        console.log(password.current.value);
+        e.preventDefault();
+
+        //create obj && send to api
+    }
     return (
         <div>
-            App
+            <form onSubmit={onSubmit}>
+                <input type={'text'} name={'login'} ref={login}/>
+                <input type={'text'} name={'password'} ref={password}/>
+                <button>login</button>
+            </form>
         </div>
     );
 };

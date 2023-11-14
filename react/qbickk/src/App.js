08374b4b345c79b7adaca430e37a72d95dba8d61
shulika <illia.shulika@gmail.com>
2023-11-13 18:04:47 -0800

    lesson3-preview5 // forms відправка данних з форми через axios //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+import {savePost} from "./services/post.api.service";
 
 const App = () => {
 
@@ -7,15 +8,7 @@ const App = () => {
 
     const onSubmit = (data) => {
         console.log(data);
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-            .then((response) => response.json())
-            .then((json) => console.log(json));
+                savePost(data).then(({data})=>console.log(data))
     }
 
 

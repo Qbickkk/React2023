7bfcda6f81aa6e6620effd5871ede68e59da5cff
shulika <illia.shulika@gmail.com>
2023-11-13 18:29:00 -0800

    lesson3_2 // forms, отримуємо дані з форми за допомогою useRef та ref, через name.current.value //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const App = () => {
 
+    const name = useRef();
+    const age = useRef();
 
 
     const save = (event) => {
         event.preventDefault();
-        console.log(event.target.name.value);
-        console.log(event.target.age.value);
+        console.log(name.current.value);
+        console.log(age.current.value);
+        event.target.reset();
     }
 
     return (
 
             <form onSubmit={save}>
-                <input type={"text"} placeholder={'name'} name={'name'}/>
-                <input type={"number"} placeholder={'age'} name={'age'}/>
+                <input type={"text"} placeholder={'name'} ref={name}/>
+                <input type={"number"} placeholder={'age'} ref={age}/>
                 <button>Save</button>
             </form>
 

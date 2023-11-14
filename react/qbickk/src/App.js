fcf81aae02ca95690a14808cda62148851b7dc5b
shulika <illia.shulika@gmail.com>
2023-11-13 17:47:58 -0800

    lesson3-preview5 // forms відправка данних з форми через fetch //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -7,6 +7,15 @@ const App = () => {
 
     const onSubmit = (data) => {
         console.log(data);
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
     }
 
 

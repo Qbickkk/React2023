cecd54bd29d013276527454e727fdd61f5cb2bbc
shulika <illia.shulika@gmail.com>
2023-11-13 17:32:45 -0800

    lesson3-preview4 // forms робота з select //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -3,7 +3,7 @@ import {useForm} from "react-hook-form";
 
 const App = () => {
 
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState:{errors}} = useForm({defaultValues:{title: 'title default', body: 'body default'}});
 
     const onSubmit = (data) => {
         console.log(data);
@@ -14,8 +14,20 @@ const App = () => {
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input {...register('title', {required: true})}/>
+                {errors.title && <span>error</span>}
                 <input {...register('body')}/>
                 <button>Save Post</button>
+
+                <select {...register('userId')}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                    <option value={8}>8</option>
+                </select>
             </form>
         </div>
     );

ede7f74e4b412cdfbb757ef2f65870818c584888
shulika <illia.shulika@gmail.com>
2023-11-13 18:19:31 -0800

    lesson3_1 // forms, отримуємо дані з форми за допомогою onSubmit та name, через event.target.name.value //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,23 +1,20 @@
-import React, {useRef} from 'react';
+import React from 'react';
 
 const App = () => {
 
-    const name = useRef();
-    const age = useRef();
+
 
     const save = (event) => {
         event.preventDefault();
-        // console.log(event.target.name.value);
-        // console.log(event.target.age.value);
-        console.log(name.current.value);
-        console.log(age.current.value);
-        event.target.reset();
+        console.log(event.target.name.value);
+        console.log(event.target.age.value);
     }
+
     return (
 
             <form onSubmit={save}>
-                <input type='text' placeholder={'name'} ref={name}/>
-                <input type='number' placeholder={'age'} ref={age}/>
+                <input type={"text"} placeholder={'name'} name={'name'}/>
+                <input type={"number"} placeholder={'age'} name={'age'}/>
                 <button>Save</button>
             </form>
 

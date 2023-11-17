ef5efb64bc8ad7e42e781dc4c05f549031949f49
shulika <illia.shulika@gmail.com>
2023-11-16 21:40:01 -0800

    lesson3_1 // forms, part 1 //

--- react/qbickk/src/App.js
+++ react/qbickk/src/App.js
@@ -1,25 +1,32 @@
-import React, { useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 const App = () => {
 
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState({});
 
-   const handler = (event) => {
-        event.preventDefault();
-        setUser(prevState => ({...prevState, [event.target.name] : event.target.value}))
+    // const name = useRef();
+    // const age = useRef();
+console.log (user)
+    const handler = (e) => {
+        e.preventDefault();
+        setUser(prev => ({...prev, [e.target.name]: e.target.value}));
+    }
 
-   }
-
-   console.log(user)
+    const save = (event) =>{
+        // event.preventDefault();
+        // console.log(event.target.name.value);
+        // console.log(event.target.age.value);
+        // console.log(name.current.value);
+        // console.log(age.current.value);
+        // event.target.reset()
+    }
 
     return (
-
-            <form>
-                <input type={"text"} placeholder={'name'} name={'name'} onChange={handler}/>
-                <input type={"number"} placeholder={'age'} name={'age'}  onChange={handler}/>
-                <button>Save</button>
-            </form>
-
+        <form onSubmit={null}>
+            <input type={'text'} placeholder={'name'} name={'name'} onChange={handler}/>
+            <input type={'text'} placeholder={'age'}  name={'age'} onChange={handler}/>
+            <button>Save</button>
+        </form>
     );
 };
 

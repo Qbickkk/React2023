fb3f09bc758d3ab1a8c2089bb56c6d390dd4788d
shulika <illia.shulika@gmail.com>
2023-12-17 15:49:40 -0800

    lesson7-preview5 redux_toolkit useDispatch

--- react/qbickk/src/components/UsersContainer/Users.js
+++ react/qbickk/src/components/UsersContainer/Users.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {addUser} from "../../store ";
 
 const Users = () => {
     const {users} = useSelector(store=>store.user);
+    const dispatch = useDispatch();
+
+    const submit = (event) => {
+        event.preventDefault();
+        const name = event.target.userName.value;
+        const user = {
+            id: new Date().getTime(), name
+        };
+        dispatch(addUser({user}));
+    };
+
     return (
         <div>
+            <form onSubmit={submit}>
+                <input type={'text'} placeholder={'name'} name={'userName'}/>
+                <button>Save</button>
+            </form>
             {users.map(user=><div key={user.id}>{user.name}</div>)}
         </div>
     );
--- react/qbickk/src/store /slices/user.slice.js
+++ react/qbickk/src/store /slices/user.slice.js
@@ -10,9 +10,16 @@ const initialState = {
 
 const userSlice = createSlice({
     name: 'userSlice',
-    initialState
+    initialState,
+    reducers:{
+        addUser: (state,action)=>{
+            const user = action.payload.user;
+            state.users.push(user)
+        }
+    }
 });
 
 const userReducer = userSlice.reducer;
+export const {addUser} = userSlice.actions;
 
 export default userReducer;
